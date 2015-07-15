4c9019242587ea7af109548023cb9d5a914fd53a
nickgenov <nickolay.r.genov@hotmail.com>
2015-07-15 17:21:24 +0300

    Entity framework

---
+++ 01.Database-Applications-Entity-Framework/01.DatabaseFirst/DatabaseFirst.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.DatabaseFirst
+{
+    class DatabaseFirst
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new NorthwindEntities())
+            {
+                var employees = context.Employees
+                    .Where(e => e.FirstName == "Andrew")
+                    .Select(e => e.Address);
+
+                foreach (var e in employees)
+                {
+                    Console.WriteLine(e);
+                }
+            }
+
+        }
+    }
+}
