7e9a429d204b5e1738e92259f65fadc6f48473dd
nickgenov <nickolay.r.genov@gmail.com>
2015-07-19 23:35:23 +0300

    Entity framework code first practice

---
+++ 02.Entity-Framework-Code-First/02.Entity-Framework-Code-First-Practice/ForumSystem.Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ForumSystem.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Gender Gender { get; set; }
+        public DateTime RegisteredOn { get; set; }
+        public DateTime? Birthday { get; set; }
+    }
+}
