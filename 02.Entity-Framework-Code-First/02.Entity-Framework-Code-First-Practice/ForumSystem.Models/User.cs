15458fc7bef319c3ccfa459a125f1a8f45eab831
nickgenov <nickolay.r.genov@gmail.com>
2015-07-20 01:21:54 +0300

    Practice

--- 02.Entity-Framework-Code-First/02.Entity-Framework-Code-First-Practice/ForumSystem.Models/User.cs
+++ 02.Entity-Framework-Code-First/02.Entity-Framework-Code-First-Practice/ForumSystem.Models/User.cs
@@ -1,16 +1,30 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ForumSystem.Models
 {
     public class User
     {
+        private ICollection<Question> questions;
+
+        public User()
+        {
+            this.questions = new HashSet<Question>();
+        }
+
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
 
-        //public string Username { get; set; }
+        public string Username { get; set; }
+        public int? Age { get; set; }
         public Gender Gender { get; set; }
         public DateTime RegisteredOn { get; set; }
         public DateTime? Birthday { get; set; }
+        public virtual ICollection<Question> Questions
+        {
+            get { return this.questions; }
+            set { this.questions = value; }
+        }
     }
 }
