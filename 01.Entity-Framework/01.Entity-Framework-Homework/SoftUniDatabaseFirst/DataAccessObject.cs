c0ff149d562e7cb3e889e88013c3c8d95e608d7e
nickgenov <nickolay.r.genov@gmail.com>
2015-07-20 23:59:33 +0300

    Entity Framework model first homework

---
+++ 01.Entity-Framework/01.Entity-Framework-Homework/SoftUniDatabaseFirst/DataAccessObject.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Data.Entity.Core;
+using System.Linq;
+
+namespace SoftUniDatabaseFirst
+{
+    public class DataAccessObject
+    {
+        public static void Add(Employee employee)
+        {
+            using (var context = new SoftUniEntities())
+            {
+                context.Employees.Add(employee);
+                context.SaveChanges();
+                Console.WriteLine("Employee added");
+            }
+        }
+
+        public static Employee FindByKey(object key)
+        {
+            using (var context = new SoftUniEntities())
+            {
+                var employee = context.Employees.Find(key);
+                if (employee == null)
+                {
+                    throw new ObjectNotFoundException("Employee not found");
+                }
+                return employee;
+            }
+        }
+
+        public static void Modify(Employee employee, string name)
+        {
+            using (var context = new SoftUniEntities())
+            {
+                var emp = context.Employees.Find(employee.EmployeeID);
+
+                emp.FirstName = name;
+                context.SaveChanges();
+                Console.WriteLine("FirstName changed");
+            }
+        }
+
+        public static void Delete(Employee employee)
+        {
+            using (var context = new SoftUniEntities())
+            {
+                var emp = context.Employees.Find(employee.EmployeeID);
+
+                var projects = emp.Projects;
+                while (projects.Count > 0)
+                {
+                    emp.Projects.Remove(projects.First());
+                }
+
+                context.Employees.Remove(emp);
+                context.SaveChanges();
+                Console.WriteLine("Employee deleted");
+            }
+        }
+    }
+}
