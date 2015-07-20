0ace766d90dfef8fb711bfc22dfe1b2de1a660ec
nickgenov <nickolay.r.genov@hotmail.com>
2015-07-20 16:53:40 +0300

    Homework

---
+++ 01.Entity-Framework/01.Entity-Framework-Homework/SoftUniDBFirst/DataAccessObject.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftUniDBFirst
+{
+    class DataAccessObject
+    {
+        public static void Add(Employee employee) 
+        {
+            using (var context = new SoftUniEntities())
+            {
+                context.Employees.Add(employee);
+                context.SaveChanges();
+            }
+        }
+        public static void FindByKey(Object key)
+        {
+            using (var context = new SoftUniEntities())
+            {
+                var employee = context.Employees.Find(key);
+                Console.WriteLine("{0} {1} - salary: {2}", employee.FirstName, employee.LastName, employee.Salary);
+            }
+        }
+        public static void Modify(Employee employee) 
+        {
+            using (var context = new SoftUniEntities())
+            {
+                
+            }
+        }
+    }
+}
