dd8112324d72cb16df32b418c6f72a9857b6cad0
nickgenov <nickolay.r.genov@gmail.com>
2015-07-23 18:11:37 +0300

    Entity framework performance homework

---
+++ 03.Entity-Framework-Performance/Homework/ConsoleClient/Client.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleClient
+{
+    class Client
+    {
+        static void Main()
+        {
+            using (var context = new AdsEntities())
+            {
+                //Problem 1. Show Data from Related Tables
+
+                //Using Entity Framework write a SQL query to select all ads from the database and later print their title, status, category, town and user. Do not use Include(…) for the relationships of the Ads. Check how many SQL commands are executed with the SQL ExpressProfiler (or a similar tool).
+
+                var adsSelected = context.Ads
+                    .Select(a => new
+                    {
+                        a.Title,
+                        a.AdStatus.Status,
+                        Category = a.Category.Name,
+                        Town = a.Town.Name,
+                        User = a.AspNetUser.Name
+                    });
+
+                //One SQL command is sent:
+                foreach (var ad in adsSelected)
+                {
+                    Console.WriteLine("Title: {0}, status: {1}, category: {2}, town: {3}, user: {4}", ad.Title, ad.Status, ad.Category, ad.Town, ad.User);
+                }
+
+                var ads = context.Ads;
+
+                //Many SQL commands are sent:
+                foreach (var ad in ads)
+                {
+                    Console.WriteLine("Title: {0}, status: {1}, category: {2}, town: {3}, user: {4}",
+                        ad.Title,
+                        ad.AdStatus.Status,
+                        (ad.CategoryId != null) ? ad.Category.Name : "no data",
+                        (ad.TownId != null) ? ad.Town.Name : "no data",
+                        ad.AspNetUser.Name);
+                }
+
+                //Add Include(…) to select statuses, categories, towns and users along with all ads. Compare the number of executed SQL statements and the performance before and after adding Include(…).
+
+                foreach (var ad in context.Ads
+                    .Include(a => a.AdStatus)
+                    .Include(a => a.Category)
+                    .Include(a => a.Town)
+                    .Include(a => a.AspNetUser))
+                {
+                    Console.WriteLine("Title: {0}, status: {1}, category: {2}, town: {3}, user: {4}",
+                        ad.Title,
+                        ad.AdStatus.Status,
+                        (ad.CategoryId != null) ? ad.Category.Name : "no data",
+                        (ad.TownId != null) ? ad.Town.Name : "no data",
+                        ad.AspNetUser.Name);
+                }
+            }
+/*
++--------------------------+---------------+-----------------+
+|                          | No Include(…) | With Include(…) |
++--------------------------+---------------+-----------------+
+| Number of SQL statements | 30+           |               1 |
++--------------------------+---------------+-----------------+
+*/
+
+            using (var context = new AdsEntities())
+            {
+                //Problem 2. Play with ToList()
+
+                //Using Entity Framework select all ads from the database, then invoke ToList(), then filter the ads whose status is Published; then select the ad title, category and town, then invoke ToList() again and finally order the ads by publish date. 
+
+                //initialize the DB connection in order to measure more accurately
+                var adsCount = context.Ads.Count();
+                Console.WriteLine(adsCount);
+
+                var timer = new Stopwatch();
+
+                timer.Start();
+                IncorrectToListUse(context);
+                Console.WriteLine("Incorrect use of ToList() query duration: {0}", timer.Elapsed);
+
+                timer.Restart();
+                CorrectToListUse(context);
+                Console.WriteLine("Correct use of ToList() query duration: {0}", timer.Elapsed);
+            }
+/*
++--------------------+-------+-------+-------+-------+-------+-------+-------+-------+-------+--------+--------------+
+|                    | Run 1 | Run 2 | Run 3 | Run 4 | Run 5 | Run 6 | Run 7 | Run 8 | Run 9 | Run 10 | Average time |
++--------------------+-------+-------+-------+-------+-------+-------+-------+-------+-------+--------+--------------+
+| Non-optimized (s.) | 0.095 | 0.093 | 0.055 | 0.078 | 0.092 | 0.092 | 0.087 | 0.091 | 0.087 | 0.094  | 0.086        |
+| Optimized (s.)     | 0.011 | 0.013 | 0.016 | 0.027 | 0.022 | 0.016 | 0.014 | 0.015 | 0.011 | 0.011  | 0.015        |
++--------------------+-------+-------+-------+-------+-------+-------+-------+-------+-------+--------+--------------+
+*/
+            using (var context = new AdsEntities())
+            {
+                //Problem 3. Select Everything vs. Select Certain Columns
+
+                //initialize the DB connection in order to measure more accurately
+                var adsCount = context.Ads.Count();
+                Console.WriteLine(adsCount);
+
+                var timer = new Stopwatch();
+
+                timer.Start();
+                //1. Select everything from the Ads table and print only the ad title:
+                SelectAllAdColumns(context);
+                Console.WriteLine("Select Ad * duration: {0}", timer.Elapsed);
+
+                timer.Restart();
+
+                //2. Select the ad title from Ads table and print it.
+                SelectOnlyAdTitle(context);
+                Console.WriteLine("Select Ad title duration: {0}", timer.Elapsed);
+            }
+/*
++--------------------+-------+-------+-------+-------+-------+-------+-------+-------+-------+--------+--------------+
+|                    | Run 1 | Run 2 | Run 3 | Run 4 | Run 5 | Run 6 | Run 7 | Run 8 | Run 9 | Run 10 | Average time |
++--------------------+-------+-------+-------+-------+-------+-------+-------+-------+-------+--------+--------------+
+| Non-optimized (s.) | 0.095 | 0.091 | 0.090 | 0.088 | 0.092 | 0.092 | 0.089 | 0.091 | 0.088 | 0.094  | 0.092        |
+| Optimized (s.)     | 0.015 | 0.012 | 0.11 | 0.012  | 0.015 | 0.016 | 0.014 | 0.015 | 0.015 | 0.015  | 0.015        |
++--------------------+-------+-------+-------+-------+-------+-------+-------+-------+-------+--------+--------------+
+*/
+        }
+
+        private static void SelectOnlyAdTitle(AdsEntities context)
+        {
+            var query = @"CHECKPOINT; DBCC DROPCLEANBUFFERS;";
+            context.Database.ExecuteSqlCommand(query);
+
+            var ads = context.Ads
+                .Select(a => a.Title);
+            foreach (var ad in ads)
+            {
+                Console.WriteLine(ad);
+            }
+        }
+
+        private static void SelectAllAdColumns(AdsEntities context)
+        {
+            var query = @"CHECKPOINT; DBCC DROPCLEANBUFFERS;";
+            context.Database.ExecuteSqlCommand(query);
+
+            foreach (var ad in context.Ads)
+            {
+                Console.WriteLine(ad.Title);
+            }
+        }
+
+        private static void CorrectToListUse(AdsEntities context)
+        {
+            var query = @"CHECKPOINT; DBCC DROPCLEANBUFFERS;";
+            context.Database.ExecuteSqlCommand(query);
+
+            var ads = context.Ads
+                .Where(a => a.AdStatus.Status == "Published")
+                .Select(a => new
+                {
+                    a.Title,
+                    Category = (a.CategoryId != null) ? a.Category.Name : "no category",
+                    Town = (a.TownId != null) ? a.Town.Name : "no town",
+                    PublishDate = a.Date
+                })
+                .OrderBy(a => a.PublishDate)
+                .ToList();
+        }
+
+        private static void IncorrectToListUse(AdsEntities context)
+        {
+            var query = @"CHECKPOINT; DBCC DROPCLEANBUFFERS;";
+            context.Database.ExecuteSqlCommand(query);
+
+            var ads = context.Ads
+                .ToList()
+                .Where(a => a.AdStatus.Status == "Published")
+                .Select(a => new
+                {
+                    a.Title,
+                    Category = (a.CategoryId != null) ? a.Category.Name : "no category",
+                    Town = (a.TownId != null) ? a.Town.Name : "no town",
+                    PublishDate = a.Date
+                })
+                .ToList()
+                .OrderBy(a => a.PublishDate);
+        }
+    }
+}
---
+++ 03.Entity-Framework-Performance/Practice/ConsoleClient/Program.cs
@@ -0,0 +1,430 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Configuration;
+using System.Diagnostics;
+using EntityFramework.Extensions;
+
+namespace ConsoleClient
+{
+    class Program
+    {
+        static void Main()
+        {
+            // // Test connection:
+
+            //using (var context = new SoftUniEntities())
+            //{
+            //    var employees = context.Employees
+            //        .Where(e => e.Department.Name == "Engineering")
+            //        .Select(e => new
+            //        {
+            //            e.FirstName,
+            //            e.LastName,
+            //            Department = e.Department.Name
+            //        });
+
+            //    foreach (var employee in employees)
+            //    {
+            //        Console.WriteLine("{0} {1} --- {2}", employee.FirstName, employee.LastName, employee.Department);
+            //    }
+            //}
+
+            // // Multiple SQL Requests:
+
+            //using (var context = new SoftUniEntities())
+            //{
+            //    var employees = context.Employees;
+
+            //    foreach (var employee in employees)
+            //    {
+            //        Console.WriteLine("{0} {1} {2}", employee.LastName, employee.Department.Name, employee.Address.Town.Name);
+            //    }
+            //}
+
+
+            // // A single SQL request
+            //using (var context = new SoftUniEntities())
+            //{
+            //    var employees = context.Employees
+            //        .Select(e => new
+            //        {
+            //            e.LastName,
+            //            Department = e.Department.Name,
+            //            Town = e.Address.Town.Name
+            //        });
+
+            //    foreach (var employee in employees)
+            //    {
+            //        Console.WriteLine("{0} {1} {2}", employee.LastName, employee.Department, employee.Town);
+            //    }
+            //}
+
+            // Another way for the above, but less efficient:
+            //using (var context = new SoftUniEntities())
+            //{
+            //    foreach (var employee in context.Employees
+            //        .Include(e => e.Department)
+            //        .Include(e => e.Address.Town))
+            //    {
+            //        Console.WriteLine("{0} {1} {2}", employee.LastName, employee.Department.Name, employee.Address.Town.Name);
+            //    }
+            //}
+
+            //using (var context = new SoftUniEntities())
+            //{
+            //    var towns = context.Towns
+            //        .Select(t => new
+            //        {
+            //            t.Name,
+            //            t.TownID
+            //        });
+
+            //    foreach (var town in towns)
+            //    {
+            //        Console.WriteLine("{0}, {1}", town.TownID, town.Name);
+            //    }
+            //}
+
+            //using (var context = new SoftUniEntities())
+            //{
+            //    var employees = context.Employees
+            //        .OrderByDescending(e => e.FirstName)
+            //        .Take(3);
+
+            //    foreach (var employee in employees)
+            //    {
+            //        Console.WriteLine("name: {0} department: {1} town: {2}",
+            //            employee.LastName,
+            //            employee.Department.Name,
+            //            employee.Address.Town.Name);
+            //    }
+            //}
+
+            //using (var context = new SoftUniEntities())
+            //{
+            //    var employees = context.Employees
+            //        .OrderByDescending(e => e.LastName)
+            //        //YOU CAN ONLY INCLUDE WHOLE TABLES!
+            //        .Include(e => e.Department)
+            //        .Include(e => e.Address.Town)
+            //        .Take(3);
+
+            //    foreach (var employee in employees)
+            //    {
+            //        Console.WriteLine("name: {0} department: {1} town: {2}",
+            //            employee.LastName,
+            //            employee.Department.Name,
+            //            employee.Address.Town.Name);
+            //    }
+            //}
+
+            //using (var context = new SoftUniEntities())
+            //{
+            //    var employees = context.Employees
+            //        .OrderByDescending(e => e.LastName)
+            //        .Include(e => e.Department)
+            //        .Include(e => e.Address.Town)
+            //        .Take(3)
+            //        .Select(e => new
+            //        {
+            //            e.LastName,
+            //            Department = e.Department.Name,
+            //            Town = e.Address.Town.Name
+            //        });
+
+            //    foreach (var employee in employees)
+            //    {
+            //        Console.WriteLine("name: {0} department: {1} town: {2}",
+            //            employee.LastName,
+            //            employee.Department,
+            //            employee.Town);
+            //    }
+            //}
+
+
+            // //Correct use of .ToList()
+            //using (var context = new SoftUniEntities())
+            //{
+            //    var employeesFromRedmond = context.Employees
+            //        .Where(e => e.Address.Town.Name == "Redmond")
+            //        .Select(e => new
+            //        {
+            //            e.FirstName,
+            //            e.LastName,
+            //            e.JobTitle,
+            //            e.Address.AddressText
+            //        })
+            //        .OrderBy(e => e.LastName)
+            //        .ToList();
+
+            //    foreach (var emp in employeesFromRedmond)
+            //    {
+            //        Console.WriteLine("{0} {1} ({2}), address: {3}", emp.FirstName, emp.LastName, emp.JobTitle, emp.AddressText);
+            //    }
+            //}
+
+            //using (var context = new SoftUniEntities())
+            //{
+            //    var employeesFromCalgary = context.Employees
+
+            //        //INCORRECT, SHOULD BE AT THE END!!!
+            //        .ToList()
+
+            //        .Where(e => e.Address.Town.Name == "Calgary")
+            //        .Select(e => new
+            //        {
+            //            e.FirstName,
+            //            e.LastName,
+            //            e.JobTitle,
+            //            e.Address.AddressText
+            //        })
+            //        .OrderBy(e => e.LastName);
+
+            //    foreach (var emp in employeesFromCalgary)
+            //    {
+            //        Console.WriteLine("{0} {1} ({2}), address: {3}", emp.FirstName, emp.LastName, emp.JobTitle, emp.AddressText);
+            //    }
+            //}
+
+            //using (var context = new SoftUniEntities())
+            //{
+            //    decimal totalSalaries = 0;
+
+            //    foreach (var salary in context.Employees
+            //    {
+            //        totalSalaries += salary;
+            //    }
+
+            //    Console.WriteLine("{0:F2}", totalSalaries);
+            //}
+
+            //using (var context = new SoftUniEntities())
+            //{
+            //    decimal totalSalaries = 0;
+
+            //    foreach (var salary in context.Employees
+            //        .Select(e => e.Salary))
+            //    {
+            //        totalSalaries += salary;
+            //    }
+
+            //    Console.WriteLine("{0:F2}", totalSalaries);
+            //}
+
+            //using (var context = new SoftUniEntities())
+            //{
+            //    var totalSalaries = context.Employees
+            //        .Sum(e => e.Salary);
+
+            //    var avgSalaries = context.Employees
+            //        .Average(e => e.Salary);
+
+            //    Console.WriteLine("Total salaries: {0:F2}", totalSalaries);
+            //    Console.WriteLine("Average salary: {0:F2}", avgSalaries);
+            //}
+
+            //using (var context = new SoftUniEntities())
+            //{
+            //    var employeesProjects = context.Employees
+            //        .Where(e => e.Address.Town.Name == "Redmond")
+            //        .Select(e => new
+            //        {
+            //            e.FirstName,
+            //            e.LastName,
+            //            Town = e.Address.Town.Name,
+            //            Department = e.Department.Name,
+            //            Manager = e.Manager.LastName,
+            //            Projects = e.Projects.Select(p => new
+            //            {
+            //                Project = p.Name,
+            //                p.Description,
+            //                AvgSalary = p.Employees.Average(emp => emp.Salary)
+            //            })
+            //        })
+            //        .Take(10)
+            //        .OrderByDescending(e => e.Department)
+            //        .ToList();
+
+            //    foreach (var emp in employeesProjects)
+            //    {
+            //        Console.WriteLine("----- {0} {1} from {2}, department: {3}, manager: {4}", emp.FirstName, emp.LastName, emp.Town, emp.Department, emp.Manager);
+
+            //        foreach (var project in emp.Projects)
+            //        {
+            //            Console.WriteLine("{0}, average employee salary: {1}", project.Project, project.AvgSalary);
+            //        }
+            //    }
+            //}
+
+            // UPDATING Entities in EF
+            //using (var context = new SoftUniEntities())
+            //{
+            //    var timer = new Stopwatch();
+
+            //    timer.Start();
+            //    EfStandartUpdate(context);
+            //    Console.WriteLine("EF query: {0}", timer.Elapsed); //00:00:00.2239094
+
+            //    timer.Restart();
+
+            //    timer.Start();
+            //    NativeUpdate(context);
+            //    Console.WriteLine("Native query: {0}", timer.Elapsed); //00:00:00.0046463
+            //}
+
+            // UPDATING Entities with EF Extensions
+            //using (var context = new SoftUniEntities())
+            //{
+            //    context.Employees
+            //        .Where(e => e.FirstName == "Jacomo")
+            //        .Update(e => new Employee()
+            //        {
+            //            FirstName = "Roberto"
+            //        });
+
+            //    context.SaveChanges();
+            //}
+
+            //using (var context = new SoftUniEntities())
+            //{
+            //    context.Employees
+            //        .Update(e => new Employee()
+            //        {
+            //            Salary = 600000
+            //        });
+
+            //    context.SaveChanges();
+            //}
+
+            // DELETING WITH EF Extensions
+            //using (var context = new SoftUniEntities())
+            //{
+            //    var employees = context.Employees
+            //        .Where(e => e.Address.Town.Name == "Sofia")
+            //        .Select(e => new
+            //        {
+            //            e.FirstName,
+            //            e.LastName,
+            //            Address = e.Address.AddressText,
+            //            Town = e.Address.Town.Name
+            //        });
+
+            //    foreach (var employee in employees)
+            //    {
+            //        Console.WriteLine("{0} {1} --- {2}, {3}", employee.FirstName, employee.LastName, employee.Address, employee.Town);
+            //    }
+
+            //    context.Employees
+            //        .Where(e => e.Address.Town.Name == "Sofia")
+            //        .Delete();
+
+            //    context.SaveChanges();
+            //}
+
+            // Comparing the three different update methods Speed:
+            //using (var context = new SoftUniEntities())
+            //{
+            //    var employeesCount = context.Employees.Count();
+            //    Console.WriteLine("Number of employees: {0}", employeesCount);
+
+            //    Stopwatch timer = new Stopwatch();
+                
+            //    timer.Start();
+            //    EfStandartUpdate(context);
+            //    Console.WriteLine("EF Standard update: {0}", timer.Elapsed);
+
+            //    timer.Restart();
+            //    EfExtensionsUpdate(context);
+            //    Console.WriteLine("EF Extensions update: {0}", timer.Elapsed);
+
+            //    timer.Restart();
+            //    NativeUpdate(context);
+            //    Console.WriteLine("Native update: {0}", timer.Elapsed);
+
+            //}
+
+            //Add a Base64 Encoded image to the database:
+            using (var context = new SoftUniEntities())
+            {
+                for (int i = 0; i < 10000; i++)
+                {
+                    context.Images.Add(new Image()
+                    {
+                        ImageBase64 = new string('-', 5) + i
+                    });
+                }
+                context.SaveChanges();
+
+                var imageCount = context.Images.Count();
+                Console.WriteLine(imageCount);
+
+                var timer = new Stopwatch();
+                timer.Start();
+
+                EfStandartDelete(context);
+                Console.WriteLine("EF Standard delete: {0}", timer.Elapsed);
+                timer.Restart();
+
+                EfExtensionsDelete(context);
+                Console.WriteLine("EF Extensions delete: {0}", timer.Elapsed);
+                timer.Restart();
+
+                NativeDelete(context);
+                Console.WriteLine("Native delete: {0}", timer.Elapsed);
+                timer.Restart();
+            }
+        }
+
+        private static void EfStandartUpdate(SoftUniEntities context)
+        {
+            var employees = context.Employees;
+
+            foreach (var employee in employees)
+            {
+                employee.Salary = 520000;
+            }
+            context.SaveChanges();
+        }
+        private static void EfExtensionsUpdate(SoftUniEntities context)
+        {
+            context.Employees
+                .Update(e => new Employee()
+                {
+                    Salary = 850000
+                });
+            context.SaveChanges();
+        }
+        private static void NativeUpdate(SoftUniEntities context)
+        {
+            var query = @"UPDATE Employees SET Salary = 250000";
+            context.Database.ExecuteSqlCommand(query);
+            context.SaveChanges();
+        }
+        private static void EfStandartDelete(SoftUniEntities context)
+        {
+            var images = context.Images
+                .Where(i => i.ImageBase64.EndsWith("5"));
+            foreach (var image in images)
+            {
+                context.Images.Remove(image);
+            }
+            context.SaveChanges();
+        }
+        private static void EfExtensionsDelete(SoftUniEntities context)
+        {
+            context.Images
+                .Where(i => i.ImageBase64.EndsWith("6"))
+                .Delete();
+            context.SaveChanges();
+        }
+        private static void NativeDelete(SoftUniEntities context)
+        {
+            var query = @"DELETE FROM Images WHERE Images.ImageBase64 LIKE '%3'";
+            context.Database.ExecuteSqlCommand(query);
+            context.SaveChanges();
+        }
+    }
+}
